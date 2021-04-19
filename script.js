100a2b8dcd19e735a32b91cfff5bd87a7e6246a5
cassienoelle <cassienoelle@gmail.com>
2021-04-19 05:32:25 -0700

    init layout

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+
+$(document).ready(function() {
+  // init Isotope
+  let $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer'
+    }
+  });
+
+  // layout Isotope after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.isotope('layout');
+  });
+
+  // let $textBox = $('.text-box');
+  // $textBox.height($textBox.width());
+
+
+});
